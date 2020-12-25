5fb457845736f4ec6a663782f07be51a1e00bc4c
Edward Tang <edwardtang@Edwards-MacBook-Pro.local>
2020-12-25 12:17:52 -0800

    chapter 12 before disallowing empty input

--- Checklist2/AddItemViewController.swift
+++ Checklist2/AddItemViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
     }
 
+    
     @IBAction func cancel(){
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done(){
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableVIew: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 }
