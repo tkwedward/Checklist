a31c9c0c2d09a46218241d94b87e2b113686c01d
Edward Tang <edwardtang@Edwards-MacBook-Pro.local>
2020-12-25 12:28:07 -0800

    chapter 12 end

--- Checklist2/AddItemViewController.swift
+++ Checklist2/AddItemViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +36,19 @@ class AddItemViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
+    }
+    
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
 }
