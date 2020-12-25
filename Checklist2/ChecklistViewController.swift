b0c3200168f1f3e988614823300c12d6c3078040
Edward Tang <edwardtang@Edwards-MacBook-Pro.local>
2020-12-25 01:01:11 -0800

    chapter 9 finished

---
+++ Checklist2/ChecklistViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Checklist2
+//
+//  Created by Edward Tang on 12/25/20.
+//  Copyright © 2020 Edward Tang. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK:- Table View Data Source
+    override func tableView(_ tableVIew: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice-cream"
+        }
+        return cell
+    }
+}
+
